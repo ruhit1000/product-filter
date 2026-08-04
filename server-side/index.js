7445b79e8513269f3a868a30ff10a84ab23e01d2
Ruhit Paul <ruhitpaul18@gmail.com>
2026-08-04 17:37:34 +0600

    feat: integrate Tailwind CSS and add API endpoint for filters
    - Added Tailwind CSS and its Vite plugin to the client-side dependencies.
    - Imported Tailwind CSS in the index.css file.
    - Updated Vite configuration to include Tailwind CSS plugin.
    - Modified MongoDB client configuration to set strict mode to false.
    - Implemented a new API endpoint '/api/filters' to fetch distinct product attributes (brands, display types, sizes, chipsets, RAM, storage, battery) and return them sorted.

--- server-side/index.js
+++ server-side/index.js
@@ -14,7 +14,7 @@ const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
-    strict: true,
+    strict: false,
     deprecationErrors: true,
   }
 });
@@ -36,6 +36,33 @@ async function run() {
         res.status(500).json({ error: 'Internal Server Error' });
       }
     });
+
+    app.get('/api/filters', async (req, res) => {
+      try {
+        const collection = productsCollection;
+
+        const brands = await collection.distinct('brand');
+        const displayTypes = await collection.distinct('displayType');
+        const displaySizes = await collection.distinct('displaySize');
+        const chipsets = await collection.distinct('chipset');
+        const ram = await collection.distinct('ram');
+        const storage = await collection.distinct('internalStorage');
+        const battery = await collection.distinct('battery');
+
+        res.json({ 
+          brands : brands.sort(),
+          displayTypes : displayTypes.sort(),
+          displaySizes : displaySizes.sort((a, b) => a - b),
+          chipsets : chipsets.sort(),
+          ram : ram.sort((a, b) => a - b),
+          storage : storage.sort((a, b) => a - b),
+          battery : battery.sort((a, b) => a - b)
+         });
+      } catch (error) {
+        console.error('Error fetching filters:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    })
 
 
   } finally {
